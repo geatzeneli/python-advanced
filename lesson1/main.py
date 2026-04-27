6db194833625444e7a53540ebebf224d8ace6328
geatzeneli <geatzeneli03@gmail.com>
2026-04-27 19:18:52 +0200

    lesson1

---
+++ lesson1/main.py
@@ -0,0 +1,20 @@
+name="Geat"
+print(name)
+
+name="Gert"
+print(name)
+
+x=28
+y=23
+print(x-y)
+
+numer="20"
+konvertimi = int(numer)
+print(konvertimi)
+
+print(type(x))
+print(type(name))
+
+numberFloat=10.5
+print(numberFloat)
+print(type(numberFloat))
